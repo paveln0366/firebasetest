49845aac04b540c150388908b6148f653f68cfe5
Pavel <paveln0366@gmail.com>
2021-02-11 20:16:30 +0300

    implemented adding and reading data

--- app/src/main/java/com/pavelpotapov/firebasetest/MainActivity.java
+++ app/src/main/java/com/pavelpotapov/firebasetest/MainActivity.java
@@ -1,14 +1,71 @@
 package com.pavelpotapov.firebasetest;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = FirebaseFirestore.getInstance();
+
+//        Map<String, Object> user = new HashMap<>();
+//        user.put("first_name", "Pavel");
+//        user.put("last_name", "Potapov");
+//        user.put("age", 26);
+//
+//        db.collection("users")
+//                .add(user)
+//                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+//                    @Override
+//                    public void onSuccess(DocumentReference documentReference) {
+//                        Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Toast.makeText(MainActivity.this, "Error: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+//                    }
+//                });
+
+        db.collection("users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                            QuerySnapshot querySnapshot = task.getResult();
+                            if (querySnapshot == null) return;
+                            for (QueryDocumentSnapshot documentSnapshot : querySnapshot) {
+                                Map<String, Object> user = documentSnapshot.getData();
+                                Log.i("firestore_test", user.get("first_name").toString());
+                                Log.i("firestore_test", user.get("last_name").toString());
+                                Log.i("firestore_test", user.get("age").toString());
+                            }
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error: " + task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
